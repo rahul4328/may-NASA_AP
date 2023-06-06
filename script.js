cb443191a62fff539bac3641cdf6d0868d83699f
rahul4328 <125386949+rahul4328@users.noreply.github.com>
2023-06-06 11:12:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,131 +1,99 @@
-var url = `https://api.nasa.gov/planetary/apod?api_key=LCc8yC3V8qH2zpKDNlqx2G9jEKIw2kwPOhuNCX2a&date=`;
-const loadingElement = document.getElementById('loading');
+// Function to fetch and display the image of the current date
+function getCurrentImageOfTheDay() {
+    const apiKey = "cbXGhNU14iewaobY2vpRzFvfZeLsLGWVVs9YgbhP";
+    const currentDate = new Date().toISOString().slice(0, 10);
 
-getCurrentImageOfTheDay();
-//  fetch the data for the current date
+    fetch(`https://api.nasa.gov/planetary/apod?date=${currentDate}&api_key=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+            // Display data in the UI
+            const currentImageContainer = document.getElementById("current-image-container");
+            currentImageContainer.innerHTML = `
+            <h1>NASA Picture of the Day</h1>
+            <img src="${data.url}" alt="${data.title}">
+            <h3>${data.title}</h3>
+            <p>${data.explanation}</p>
+        `;
+        })
+        .catch(error => console.log(error));
+}
 
-loadingElement.style.display = 'block';
+// Function to fetch and display the image for the selected date
+function getImageOfTheDay(selectedDate) {
+    const apiKey = "cbXGhNU14iewaobY2vpRzFvfZeLsLGWVVs9YgbhP";
 
-function getCurrentImageOfTheDay(){
+    fetch(`https://api.nasa.gov/planetary/apod?date=${selectedDate}&api_key=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+            // Format the selectedDate for display
+            const formattedDate = new Date(selectedDate).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+
+            // Display data in the UI
+            const currentImageContainer = document.getElementById("current-image-container");
+            currentImageContainer.innerHTML = `
+                <h1>NASA Picture on ${formattedDate}</h1>
+                <img src="${data.url}" alt="${data.title}">
+                <h3>${data.title}</h3>
+                <p>${data.explanation}</p>
+            `;
+
+            // Save the date to local storage and add it to the search history
+            saveSearch(selectedDate);
+            addSearchToHistory(selectedDate);
+        })
+        .catch(error => console.log(error));
+}
+
+
+// Function to save a date to local storage
+function saveSearch(date) {
+    const searches = JSON.parse(localStorage.getItem("searches")) || [];
+    searches.push(date);
+    localStorage.setItem("searches", JSON.stringify(searches));
+}
+
+// // Function to add a date to the search history
+// function addSearchToHistory(date) {
+//     const searchHistory = document.getElementById("search-history");
+//     const li = document.createElement("li");
+//     li.textContent = date;
+//     li.addEventListener("click", function() {
+//         // Fetch and display the image for the clicked date
+//         getImageOfTheDay(date);
+//     });
+//     searchHistory.appendChild(li);
+// }
+
+function addSearchToHistory(date) {
+    const searchHistory = document.getElementById("search-history");
+    const li = document.createElement("li");
+    const link = document.createElement("a");
+    link.href = "javascript:void(0)";
+    link.textContent = date;
+    link.addEventListener("click", function() {
+        // Fetch and display the image for the clicked date
+        getImageOfTheDay(date);
+    });
+    li.appendChild(link);
+    searchHistory.appendChild(li);
+}
+
+
+// Event listener for form submission
+document.getElementById("search-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const selectedDate = document.getElementById("search-input").value;
     const currentDate = new Date().toISOString().split("T")[0];
-    
-    var playWithData = fetchData(url,currentDate)//play with waiting  
-    playWithData.then(responseData=>responseData.json())
-    .then(data=>{
-        loadingElement.style.display = 'none';
-        displayMethod(data,"NASA Picture Of The Day")
-        addSearchToHistory(getDataLocalStorage);
-        document.getElementById("input-date").setAttribute("max",currentDate)
-    })
-    .catch(error=>{
-        loadingElement.style.display = 'none';
-        console.log("data not Found Error")
-    });
-    
-}
 
-
-
-// display method
-function displayMethod(data,heading){
-    // console.log(data,"this data run")
-    const cardDiv = document.getElementById("card-image-data")
-    const div = `
-    <div class="content">
-        <h3>${heading} </h3> 
-        <img src="${data.url}" alt="" class="image" id="image">
-        <h3>${data.title} </h3> 
-        <p>${data.explanation}</p>
-    </div>`
-    cardDiv.innerHTML=div;
-
-}
-
-
-// fetching method 
-async function fetchData(url,date){
-    url=url+date;
-    var run = await fetch(url)
-    
-    return run;
-}
-
-
-// fetch the data for the selected date
-
-function getImageOfTheDay(event){
-    event.preventDefault(); //page not refresh
-    loadingElement.style.display = 'block'; 
-
-    const inputDate = document.getElementById("input-date").value;
-    var playWithData = fetchData(url,inputDate)//play with waiting  
-    playWithData.then(responseData=>responseData.json())
-    .then(data=>{
-        loadingElement.style.display = 'none';
-        displayMethod(data,`Picture On ${data.date}`)
-        // save date in localStorage
-        let dateThis = {
-            "date":data.date
-        }
-         saveSearch(dateThis);
-         
-    })
-    .catch(error=>console.log("This date No Data Found"));
-
-}
-function saveSearch(setDateVal){
-    //  save in localStorage
-     var arr = [];
-    if(!localStorage.getItem("searches")){
-        
-        arr.push(setDateVal)
-        
-        setDataLocalStorage(arr)
-    }else{
-       
-        arr.push(...JSON.parse(localStorage.getItem("searches")));
-        arr.push(setDateVal)
-        
-        setDataLocalStorage(arr);
+    if (selectedDate > currentDate) {
+        // Display an error message or take appropriate action
+        alert("Invalid date selection. Please choose a date on or before the current date.");
+        return;
     }
-    addSearchToHistory(getDataLocalStorage);
-}
 
-function setDataLocalStorage(arr){
-    localStorage.setItem("searches",JSON.stringify(arr)); 
-}
-function getDataLocalStorage(){
-    return JSON.parse(localStorage.getItem("searches")) 
-}
+    getImageOfTheDay(selectedDate);
+});
 
-function addSearchToHistory(localStorageData){
-    console.log(localStorageData())
-    let val = localStorageData();
-    const showLinkDiv = document.getElementById("showLink");
-    showLinkDiv.innerHTML="";
-    console.log(val[0])
-    val.forEach(element => {
-        const div = `<li onclick="showThatDate('${element.date}')">${element.date}</li>`
-        showLinkDiv.innerHTML +=div;
 
-    });
-    
-}
-function showThatDate(date){
-    loadingElement.style.display = 'block';
-    var playWithData = fetchData(url,date)//play with waiting  
-    playWithData.then(responseData=>responseData.json())
-    .then(data=>{
-        
-        loadingElement.style.display = 'none';
-        displayMethod(data,`Picture On ${data.date}`)
-        // save date in localStorage
-        let dateThis = {
-            "date":data.date
-        }
-        
-         
-    })
-    .catch(error=>console.log("This date No Data Found Now"));
-}
-
+// Display the image of the current date when the page loads
+getCurrentImageOfTheDay();
